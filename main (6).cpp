f8b9fa5539b102b044fb8e69828eb81f19259c21
efloppa <davidfalvarez2016@gmail.com>
2024-07-23 14:41:50 -0700

    Add files via upload
    Four loop examples :)

---
+++ main (6).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+  for (int j = 10; j <= 1000; j += 10){
+    cout << j << endl;
+  }
+
+for (int n = 1; n <= 17; n++) {
+
+  cout << "David" << endl;
+  
+}
+  
+for (int t = 10; t>= 1; t--)
+  
+  cout << t << endl;
+
+    for (int k = 5; k <= 50; k+=5) {
+        cout << k << endl;
+
+    }
+  
+return 0;
+}
